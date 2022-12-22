94fdec301c91f626127c41d97ec5fc7837ddbec1
Kirill Tregubov <31662934+KirillTregubov@users.noreply.github.com>
2022-12-22 00:21:58 -0500

    remove dist

--- backend/dist/models/prisma.model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client");
-const prisma = global.prisma || new client_1.PrismaClient();
-// Prevent multiple instances of Prisma Client in development
-if (process.env.NODE_ENV === "development")
-    global.prisma = prisma;
-exports.default = prisma;
--- backend/dist/routes/routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const user_routes_1 = __importDefault(require("./user.routes"));
-const api = (0, express_1.Router)().use(user_routes_1.default);
-exports.default = (0, express_1.Router)().use("/api/v1", api);
