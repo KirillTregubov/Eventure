8c617a0a20ba605e759d53712f1e0b0a5ce512fa
Soumil Dharaskar <soumil.dharaskar@hotmail.com>
2023-05-04 17:17:45 -0400

    Added rudimentary code for getting single event fetch function

--- platform/lib/Api.ts
+++ platform/lib/Api.ts
@@ -39,6 +39,42 @@ export async function getAllEventsRequest(
   return result.data
 }
 
+export async function getSingleEventRequest(
+  signal: AbortController['signal'] | undefined,
+  eventId: string
+) {
+  const controller = new AbortController()
+  const timeout = setTimeout(() => controller.abort(), 5000)
+
+  const response = await fetch(new URL(`${API_URL}/api/v1/events/${eventId}`), {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    signal: signal ? signal : controller.signal
+  })
+  clearTimeout(timeout)
+  if (!response.ok) {
+    if (response.status === 401) {
+      return { error: 'Forbidden' } as {
+        error: string
+        token: undefined
+      }
+    }
+    return new Error('Error getting the event')
+  }
+  const json = await response.json()
+
+  const result = Events.safeParse(json)
+  if (!result.success) {
+    console.error('Zod validation error:', result.error)
+    throw Error('Data validation error')
+  }
+
+  return result.data
+}
+
 export async function getEventsPageData(
   signal: AbortController['signal'] | undefined
 ) {
